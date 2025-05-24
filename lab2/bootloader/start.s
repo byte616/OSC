95ed71010c39afe3abe35a99e646b892b7ea6cf8
byte616 <p500297@gmail.com>
2025-05-24 21:43:35 +0800

    bootloader relocation

--- lab2/bootloader/start.s
+++ lab2/bootloader/start.s
@@ -1,3 +1,21 @@
+.section ".text.relocation"
+.global _relocation
+_relocation:
+	adr x0, .
+	ldr x1, = _loader_start
+	cmp x0, x1 // check already relocation or not
+	b.eq _start
+	ldr x2, = code_size
+
+
+_relocation_loop:
+	ldr x3, [x0], #8    // read 8 bytes from x0
+    str x3, [x1], #8    // write into x1
+    sub x2, x2, #8      // renew the length need to move
+    cbnz x2, _relocation_loop // if there still has data need to move, go back loop
+	ldr x4, = _start
+	br x4
+
 .section ".text.boot"
 .globl _start
 
@@ -16,7 +34,8 @@ primary:
 	sub x1, x1, x0 // end - begin = size of bss
 	bl memzero
 
-	mov sp, #0x40000 // set stack pointer
+	adr x2, _stack_top
+	mov sp, x2 // set stack pointer
 	bl main
 
 memzero:
